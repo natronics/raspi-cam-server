e48fbf1cdbc09ad572afa6de8c818e189bf30a9e
Nathan Bergey <nathan.bergey@gmail.com>
2016-03-10 10:05:21 -0700

    Camera: Long set of options for raspistill

--- app.py
+++ app.py
@@ -2,14 +2,33 @@ from flask import Flask, render_template, redirect, url_for
 import subprocess
 app = Flask(__name__)
 
+raspistill_cmd = [
+    "sudo",
+    "raspistill",
+    "--mode", "6",
+    "-q", "60",
+    "-w", "640",
+    "-h", "480",
+    "--nopreview"
+    "-ISO", "100",
+    "--shutter", "16000",
+    "--drc", "high",
+    "--awb", "fluorescent",
+    "--exposure", "night",
+    "-o", "static/currentimg.jpg",
+]
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
 
+
 @app.route("/snap/")
 def snap():
-    subprocess.call(["sudo", "raspistill", "--nopreview", "-w", "1296", "-h", "972", "-q", "5", "-o", "static/currentimg.jpg"])
+    subprocess.call(raspistill_cmd)
     return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
